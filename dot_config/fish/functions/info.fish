61a82aa8dde48563451a76f9fc9f7205d971fd60
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-03-23 12:09:43 +0000

    fix(fish): check if command exists before printing information

--- dot_config/fish/functions/info.fish
+++ dot_config/fish/functions/info.fish
@@ -3,6 +3,12 @@ function info
         echo "Usage: info PROGRAM"
         return 1
     end
+
+    if ! type -q $argv[1]
+        echo "Error: command not found"
+        return 2
+    end
+
     whereis $argv[1]
     $argv[1] --version || $argv[1] version || $argv[1] -version
 end
